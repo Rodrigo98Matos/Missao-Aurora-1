0beb79ea8ea14427be06592f414a6d02da97c7c4
Rodrigo Matos <rodrigo98matos@gmail.com>
2022-05-02 16:58:16 -0300

    Update main.cpp

--- Programação/OBDH_Aurora_1/src/main.cpp
+++ Programação/OBDH_Aurora_1/src/main.cpp
@@ -3,27 +3,22 @@
 #include <HTTPClient.h>
 #include "ADC.h"
 
-#include <WiFiMulti.h>
+//#include <WiFiMulti.h>
 
 
 
 #define SERVER_IP "200.137.130.88:8888"
 
-WiFiMulti wifiMulti;
+#ifndef STASSID
+#define STASSID "aldebaran"
+#define STAPSK  "21062021"
+#endif
+
+
 
 void setup() {
   Serial.begin(9600);
-  /*wifiMulti.addAP("ALBERTO E ROSELIA", "CHAMEX5050");
-  wifiMulti.addAP("aldebaran", "21062021");
-  wifiMulti.addAP("Rodrigo Matos", "aero2019");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());*/
+  
 }
 
 void loop() {
@@ -32,19 +27,29 @@ void loop() {
   double V_b2 = Vin_b2(32, V_b1,5100,3300);
   double T_b2 = ntc_10k(14);
 
- /* if ((wifiMulti.run() == WL_CONNECTED)) {
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if ((WiFi.status() == WL_CONNECTED)) {
     WiFiClient client;
     HTTPClient http;
-
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
-    http.begin(client, "http://200.137.130.88:8888"); //HTTP
+    http.begin(client,"http://200.137.130.88:8888/"); //HTTP
     http.addHeader("Content-Type", "application/json");
 
     Serial.print("[HTTP] POST...\n");
     // start connection and send HTTP header and body
-    int httpCode = http.POST("{\"equipe\":1,\"Bateria1\" : ["+String(V_b1)+", "+String(T_b1)+"], \"Bateria2\" : ["+String(V_b2)+", "+String(T_b2)+"]}");
-
+   int httpCode = http.POST("{\"equipe\":2,\"Bateria1\" : ["+String(V_b1)+", "+String(T_b1)+"], \"Bateria2\" : ["+String(V_b2)+", "+String(T_b2)+"]}");
+ 
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
@@ -63,8 +68,10 @@ void loop() {
 
     http.end();
   }
-*/
 
-  Serial.println("{\"Bateria1\" : ["+String(V_b1)+", "+String(T_b1)+"], \"Bateria2\" : ["+String(V_b2)+", "+String(T_b2)+"]}");
+
+  Serial.println("{\"equipe\":2,\"Bateria1\" : ["+String(V_b1)+", "+String(T_b1)+"], \"Bateria2\" : ["+String(V_b2)+", "+String(T_b2)+"]}");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
   delay(1000);
 }
